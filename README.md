aa96c5209edba30282ecf0454ee07dc736e8d080
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2017-09-07 14:29:29 +0300

    Updated readme and CLI project with demo

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# text-tables-dot-net
+# TextTables.NET
 Library for generating text-based tables for the .NET platform
+
+```csharp
+TextTable table = new TextTable();
+table.AddRow("TextTables.NET demo").WithColSpan(0,4);
+table.AddRow("Multiple columns", "Or rows", 1.6180).WithColSpan(0,2).WithRowSpan(1,2);
+table.AddRow(3.14, "foo", "bar");
+Console.WriteLine(table);
+// +---------------------------------------+
+// |          TextTables.NET demo          |
+// +---------------------+---------+-------+
+// |  Multiple columns   |         | 1.618 |
+// +----------+----------+ Or rows +-------+
+// |   3.14   |   foo    |         |  bar  |
+// +----------+----------+---------+-------+
+
+table.Options.Charset = TextTableCharset.GetBoxCharset();
+Console.WriteLine(table);
+// ┌───────────────────────────────────────┐
+// │          TextTables.NET demo          │
+// ├─────────────────────┬─────────┬───────┤
+// │  Multiple columns   │         │ 1.618 │
+// ├──────────┬──────────┤ Or rows ├───────┤
+// │   3.14   │   foo    │         │  bar  │
+// └──────────┴──────────┴─────────┴───────┘
+```
--- src/TextTables.TestCLI/Program.cs
+++ src/TextTables.TestCLI/Program.cs
@@ -6,9 +6,34 @@ namespace TextTables.TestCLI
     {
         static void Main(string[] args)
         {
-            var table = new TextTable();
-            table.AddRow(1, "2", 3.5);
+            TextTable table = new TextTable();
+            table.AddRow("TextTables.NET demo").WithColSpan(0,4);
+            table.AddRow("Multiple columns", "Or rows", 1.6180).WithColSpan(0,2).WithRowSpan(1,2);
+            table.AddRow(3.14, "foo", "bar");
             Console.WriteLine(table);
+
+            // +---------------------------------------+
+            // |          TextTables.NET demo          |
+            // +---------------------+---------+-------+
+            // |  Multiple columns   |         | 1.618 |
+            // +----------+----------+ Or rows +-------+
+            // |   3.14   |   foo    |         |  bar  |
+            // +----------+----------+---------+-------+
+
+            table.Options.Charset = TextTableCharset.GetBoxCharset();
+            Console.WriteLine(table);
+
+            // ┌────────────────────────────────────────┐
+            // │          TextTables.NET demo           │
+            // ├──────────┬──────────┬─────────┬────────┤
+            // │    1     │    2     │    3    │ 3.1415 │
+            // ├──────────┴──────────┼─────────┼────────┤
+            // │  Multiple columns   │         │ 1.618  │
+            // ├──────────┬──────────┤ Or rows ├────────┤
+            // │   foo    │   bar    │         │ 2.7182 │
+            // └──────────┴──────────┴─────────┴────────┘
+
+            Console.ReadLine();
         }
     }
 }
