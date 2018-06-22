98e64b593bb5cadaaf8d45eb6ac10b1aba38f86f
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2018-06-22 10:12:52 +0300

    Moved to .NET standard 1.0 (and .NET Core 1.0 for the test CLI), but kept .NET 3.5 as a secondary target

--- src/TextTables/TextTable.cs
+++ src/TextTables/TextTable.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Text.RegularExpressions;
 using TextTables.Internal;
 
 namespace TextTables
